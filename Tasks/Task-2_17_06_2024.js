822b5ceaca2119a80e6798520fc28df9d8c3721b
[monik2004] <[monikrupareliya123@gmail.com]>
2024-07-18 17:46:19 +0530

    first

---
+++ Tasks/Task-2_17_06_2024.js
@@ -0,0 +1,11 @@
+a=10
+b=20   
+c=30
+
+if (a > b && a > c ) {
+    console.log("a is the largest number");
+}else if (b>a && b>c) {
+    console.log("b is the largest number");
+}else if (c>a && c>b) {
+    console.log("c is the largest number");
+}
---
+++ Tasks/Task_17_06_2024.js
@@ -0,0 +1,4 @@
+let x = 10
+for (let i = 1; i <= 10; i++) {
+    console.log(x+" * "+i+" = "+x*i)
+}
